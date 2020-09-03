b472e064fcb89986ad3f7139c08b5fc535764633
mostafamahmood17 <taat.heritage@gmail.com>
2020-09-03 14:03:37 -0700

    wow

---
+++ src/build/precache-manifest.fcf7068764f2042e3e5034278b67b3ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d4d3c9b920cf4ee1675f7b1028c6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cdb43ccfa967cc7d485",
+    "url": "/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "bc485dc1ae388502a070",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2cdb43ccfa967cc7d485",
+    "url": "/static/js/2.386bd3f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.386bd3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc485dc1ae388502a070",
+    "url": "/static/js/main.9ecb8949.chunk.js"
+  },
+  {
+    "revision": "0424f88f94cb72420d12",
+    "url": "/static/js/runtime-main.e884a08b.js"
+  }
+]);
